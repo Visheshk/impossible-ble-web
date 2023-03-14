f2fc1d5007748108205b36f713f6f2f5ae854595
Vishesh Kumar <visheshkay@gmail.com>
2023-03-14 04:20:38 -0500

    getting number inputs to almost work

--- Ball-Possession/blockMaker.js
+++ Ball-Possession/blockMaker.js
@@ -9,6 +9,7 @@ blocks = [
 	"rest time"
 ];
 blockVals = [5, 230, 31, 543, 234, 12, 30];
+inputVals = [5, 230, 31, 543, 234, 12, 30];
 
 blocksStartX = 550;
 blocksStartY = 10;
@@ -28,26 +29,56 @@ receiversY = 50;
 connections = [];
 dataValues = ballPossess;
 
-function makeSlots() {
-	var ry = receiversY;
-	for (var r in receivers) {
-		dv = document.createElement("div");
-		dv.setAttribute('class', 'dropzone')
-		dv.setAttribute('id', 'receiver-' + r);
-		dv.textContent = receivers[r];
-		numInput = document.createElement("input");
-		numInput.setAttribute("type", "number");
-		numInput.setAttribute("class", "number-input");
-		dv.appendChild(numInput);
-		document.body.insertBefore(dv, document.getElementById("receivers"));
-		moveTarget(receiversX, ry, dv);
-		ry += dv.getBoundingClientRect().height + 10;
-		connections.push(-1);
-		// dataValues.push(-1);
+async function makeSlots() {
+	async function addReceivers() {
+		var ry = receiversY;
+		for (var r in receivers) {
+			dv = document.createElement("div");
+			dv.setAttribute('class', 'dropzone')
+			dv.setAttribute('id', 'receiver-' + r);
+			dv.textContent = receivers[r];
+			numInput = document.createElement("input");
+			numInput.setAttribute("type", "number");
+			numInput.setAttribute("class", "number-input");
+			numInput.setAttribute("value", blockVals[r]);
+			numInput.setAttribute("id", "receiver-input-" + r);
+			dv.appendChild(numInput);
+			await document.body.insertBefore(dv, document.getElementById("receivers"));
+			moveTarget(receiversX, ry, dv);
+			ry += dv.getBoundingClientRect().height + 10;
+			connections.push(-1);
+			// dataValues.push(-1);
+			// console.log(r);
+		}
+		return true;
 	}
+	async function addInputListener () {
+		const numInputElements = document.querySelectorAll(".number-input");
+		// console.log(numInputElements);
+		numInputElements.forEach( (nie) => {
+			nie.addEventListener("change", (event) => {
+				// console.log(event.target.value);
+				var nid = event.target.getAttribute("id");
+				var inputId = parseInt(nid.slice(nid.length - 1, nid.length));
+				inputVals[inputId] = parseInt(event.target.value);
+				blockVals[inputId] = parseInt(event.target.value);
+				ballPossess[inputId] = parseInt(event.target.value);
+				// console.log("bp " + ballPossess);
+				// console.log("iv " + inputVals);
+				// disconnectBlock(document.getElementById("block-" + inputId));
+				// console.log(event.target);
+				// const result = document.querySelector(".result");
+				// result.textContent = `You like ${event.target.value}`;
+			});
+		});
+		return true;
+	}
+	await addReceivers();
+	await addInputListener();
+	
+	
 }
-
-makeSlots()
+makeSlots();
 
 function makeBlocks() {
 	blocksX = blocksStartX;
@@ -94,6 +125,7 @@ function attachBlocks (parent, block) {
 	}
 	connections[parentIndex] = blockIndex;
 	ballPossess[parentIndex] = blockVals[blockIndex];
+	// console.log("bp " + ballPossess);
 	// if connections
 	//change 
 }
@@ -101,12 +133,14 @@ function attachBlocks (parent, block) {
 function disconnectBlock(block, moveBool=true) {
 	// console.log(block);
 	parentIndex = connections.indexOf(getBlockIndex(block));
-	console.log(connections);
-	console.log(parentIndex);
+	// console.log(connections);
+	// console.log("parentIndex" + parentIndex);
 	if (parentIndex != -1) {
 		connections[parentIndex] = -1;
 		dataValues[parentIndex] = originalBallPossess[parentIndex];
-		ballPossess[parentIndex] = originalBallPossess[parentIndex];
+		ballPossess[parentIndex] = inputVals[parentIndex];
+		// console.log("ballP " + ballPossess);
 	}
+
 	if (moveBool) {sendBlockHome(block)};
 }
